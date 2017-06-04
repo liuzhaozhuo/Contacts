da45386980937f8daf5de0fa7d055c4ad19d817b
liuzhaozhuo <liuzhaozhuo126@126.com>
2017-06-04 16:47:39 +0800

    Commit

--- app/src/main/java/cn/gdcjxy/contacts/MainActivity.java
+++ app/src/main/java/cn/gdcjxy/contacts/MainActivity.java
@@ -1,11 +1,14 @@
 package cn.gdcjxy.contacts;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,20 +18,24 @@ import cn.gdcjxy.contacts.fragment.ContactsFragment;
 import cn.gdcjxy.contacts.fragment.DelFragment;
 import cn.gdcjxy.contacts.fragment.SearchFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
-    private List<String>mTitle;
-    private List<Fragment>mFragment;
+    private List<String> mTitle;
+    private List<Fragment> mFragment;
+    private FloatingActionButton fab_add;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().setElevation(0);
+
         initData();
-        initView();        
+        initView();
     }
 
     private void initData() {
@@ -46,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        fab_add = (FloatingActionButton) findViewById(R.id.fab_add);
+        fab_add.setOnClickListener(this);
         mTabLayout = (TabLayout) findViewById(R.id.mTabLayout);
         mViewPager = (ViewPager) findViewById(R.id.mViewPage);
 
@@ -55,17 +64,36 @@ public class MainActivity extends AppCompatActivity {
         //设置适配器
         mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
 
-           //选中的item
+            //选中的item
             @Override
             public Fragment getItem(int position) {
                 return mFragment.get(position);
             }
 
+            //返回item的个数
             @Override
             public int getCount() {
                 return mFragment.size();
             }
+
+            //设置标题
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return mTitle.get(position);
+            }
         });
+
+        //绑定
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.fab_add:
+                startActivity(new Intent(MainActivity.this, AddActivity.class));
+                break;
+        }
+    }
 }
